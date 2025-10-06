d1a7585f360316cd5b331b518519b56ecf6a1c49
Evan <evanroyal1@gmail.com>
2025-10-06 16:42:28 -0600

    started converting html to jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,50 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark">
-          <div className="navbar-brand">
-            Simon<sup>&reg;</sup>
-          </div>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="play.html">
-                Play
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="scores.html">
-                Scores
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about.html">
-                About
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+    <div classNameName="body bg-dark text-light">
+      <header>
+        <h1 id="Title">WorkCircle</h1>
+            <nav className="d-flex justify-content-center py-3">
+                <menu className="nav nav-pills">
+                    <li className="nav-item"><NavLink className="nav-link active" to="">Home</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="login">Login</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="join_group">Join Group</NavLink></li>
+                </menu>
+            </nav>
+        </header>
 
       <main>App components go here</main>
 
-      <footer className="bg-dark text-white-50">
-        <div className="container-fluid">
-          <span className="text-reset">Author Name(s)</span>
-          <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
-            Source
-          </a>
-        </div>
-      </footer>
+      <footer>
+        <hr/>
+            <p>Author Name:</p>
+        <hr/>
+        <p><a href="https://github.com/Evan10">Evan Royal</a></p>
+    </footer>
     </div>
+     </BrowserRouter>
   );
 }
