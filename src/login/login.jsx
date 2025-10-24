887b6d25ca3d34c29a5f70e6a53a683d5e17a39a
Evan <evanroyal1@gmail.com>
2025-10-23 18:21:53 -0600

    added Logging out

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -19,6 +19,9 @@ export function Login({updateUser, user}) {
         
         if (loginInfo?.username == formData.get("username") && loginInfo?.password == formData.get("password")){
             updateUser(formData.get("username"))
+        }else{
+            alert("Incorrect Login")
+            return;
         }
 
         navigate("/");
@@ -27,7 +30,7 @@ export function Login({updateUser, user}) {
         const handleNewAccount= (e) =>{
         e.preventDefault();
         const formData = new FormData(e.target);
-        
+        console.log(e)
         if(!formData.get("username") || !formData.get("password") ){
             alert("Please fill out all required fields!");
             return;
@@ -39,32 +42,45 @@ export function Login({updateUser, user}) {
         navigate("/");
     }
 
+    const handleLogout = () => {
+        updateUser("");
+
+    }
+
     return (
         <div className="container-fluid text-center">
             <div className="container col-12">
-                <div className="col-8 offset-2 card">
-                    <div id="login" className=" card-body ">
-                        <form onSubmit={handleLogin} id="login-form" className="form-format">
-                            <div className="form-body d-flex flex-column align-items-center">
-                                <input className="row w-75" name="username" id="username" type="text" required placeholder="username" />
-                                <input className="row w-75" name="password" id="password" type="password" required placeholder="password" />
-                                <button id="login-submit" type="submit" className="row">Log in</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+                {user.length !=0 && (<div className="col-8 offset-2 card">
+                    <p>Hello {user}</p>
+                    <button onClick={handleLogout}>Log out</button>
+                </div>)}
 
-                <div className="col-8 offset-2 card">
-                    <div id="new-account" className="justify-content-center card-body ">
-                        <form onSubmit={handleNewAccount} id="login-form" className="form-format">
-                            <div className="form-body d-flex flex-column align-items-center">
-                                <input className="row w-75" id="new-username" type="text" required placeholder="username" />
-                                <input className="row w-75" id="new-password" type="password" required placeholder="password" />
-                                <button id="new-account-submit" type="submit" className="row">Create Account</button>
-                            </div>
-                        </form>
+                {user.length == 0 && (  <div>
+                    <div className="col-8 offset-2 card">
+                        <div id="login" className=" card-body ">
+                            <form onSubmit={handleLogin} id="login-form" className="form-format">
+                                <div className="form-body d-flex flex-column align-items-center">
+                                    <input className="row w-75" name="username" id="username" type="text" required placeholder="username" />
+                                    <input className="row w-75" name="password" id="password" type="password" required placeholder="password" />
+                                    <button id="login-submit" type="submit" className="row">Log in</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
+
+                    <div className="col-8 offset-2 card">
+                        <div id="new-account" className="justify-content-center card-body ">
+                            <form onSubmit={handleNewAccount} id="new-account-form" className="form-format">
+                                <div className="form-body d-flex flex-column align-items-center">
+                                    <input className="row w-75" name="username" id="new-username" type="text" required placeholder="username" />
+                                    <input className="row w-75" name="password" id="new-password" type="password" required placeholder="password" />
+                                    <button id="new-account-submit" type="submit" className="row">Create Account</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    </div>
+                 )} 
             </div>
         </div>
     );
