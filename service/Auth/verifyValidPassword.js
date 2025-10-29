21aef67ed334d97f6ff9b7e9914f3d914bb9393c
Evan <evanroyal1@gmail.com>
2025-10-29 12:55:18 -0600

    created password validation function

--- service/Auth/verifyValidPassword.js
+++ service/Auth/verifyValidPassword.js
@@ -2,8 +2,30 @@
 const MIN_PASSWORD_LENGTH = 5;
 const MAX_PASSWORD_LENGTH = 20;
 
+const REGEX_CONTAIN_CAPITAL = /[A-Z]/;
+const REGEX_CONTAIN_LOWERCASE = /[a-z]/;
+const REGEX_CONTAIN_NUMBER = /[0-9]/;
+const REGEX_CONTAIN_SYMBOL = /[!?><@#$%^&*()]/;
 
 export default function verifyValidPassword(password){
-
-
+    const reasons = [];
+    if (password.length < MIN_PASSWORD_LENGTH){
+        reasons.push(`Password is too short. Must be at least ${MIN_PASSWORD_LENGTH} characters long.`);
+    } 
+    if (password.length > MAX_PASSWORD_LENGTH){
+        reasons.push(`Password is too long. Must be at most ${MIN_PASSWORD_LENGTH} characters long.`);
+    } 
+    if(!REGEX_CONTAIN_CAPITAL.test(password)){
+        reasons.push("Password must contain an uppercase letter");
+    }
+    if(!REGEX_CONTAIN_LOWERCASE.test(password)){
+        reasons.push("Password must contain a lowercase letter");
+    }
+    if(!REGEX_CONTAIN_NUMBER.test(password)){
+        reasons.push("Password must contain a number");
+    }
+    if(!REGEX_CONTAIN_SYMBOL.test(password)){
+        reasons.push("Password must contain one of these symbols !?><@#$%^&*()");
+    }
+    return {valid:reasons.length==0, reasons:reasons};
 }
