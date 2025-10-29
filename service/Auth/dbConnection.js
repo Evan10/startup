1f35400852df002726d0c74e2f693e151ac51147
Evan <evanroyal1@gmail.com>
2025-10-29 13:02:57 -0600

    created placeholder methods for the db connection

--- service/Auth/dbConnection.js
+++ service/Auth/dbConnection.js
@@ -4,6 +4,7 @@ export default class dbConnection{
 
     constructor(credentials){
         this.credentials = credentials;
+        //connect to db
     }
 
 
@@ -11,11 +12,36 @@ export default class dbConnection{
         return false;
     }
 
-    
-
+    //return true when successfully created
+    createNewUser(username, passwordHash){
+        return false;
+    }
 
     getPasswordHash(username){
         // get hash from db
         return null;
     }
+
+
+
+    getJoinCodes(){
+        return {};    
+    }
+
+    createNewChat(chatName, chatID, user, join_code){
+        return false;
+    }
+
+    getChatData(username, chatID, startTime = null, endTime = null){
+        return {};
+    }
+
+    updateChatData(chatID, chatData){
+        return false;
+    }
+
+    deleteChat(username, chatID){
+        return false;
+    }
+
 }
--- service/Auth/testDB.js
+++ service/Auth/testDB.js
@@ -5,19 +5,46 @@
 
 export default class dbConnection{
 
-    
+
 
     constructor(credentials){
         this.credentials = credentials;
     }
 
+    isUsernameAvailable(username){
+        return false;
+    }
+
+    //return true when successfully created
+    createNewUser(username, passwordHash){
+        return false;
+    }
+
     getPasswordHash(username){
-        
+        // get hash from db
         return null;
     }
 
 
 
+    getJoinCodes(){
+        return {};    
+    }
 
+    createNewChat(chatName, chatID, user, join_code){
+        return false;
+    }
+
+    getChatData(username, chatID, startTime = null, endTime = null){
+        return {};
+    }
+
+    updateChatData(chatID, chatData){
+        return false;
+    }
+
+    deleteChat(username, chatID){
+        return false;
+    }
 
 }
