fc4d58cc5e68e070a893a0d1f395c49628306f72
Evan <evanroyal1@gmail.com>
2025-11-12 19:50:24 -0700

    started implementing functions to get data

--- service/dbConnection.js
+++ service/dbConnection.js
@@ -1,51 +1,102 @@
-import {MongoClient} from mongodb;
+import { MongoClient } from mongodb;
 
 export default class dbConnection{
+
+
+    async testConnection(db){
+        try {
+            await db.command({ ping: 1 });
+            console.log(`DB connected successfully`);
+        } catch (ex) {
+            console.log(`Error connecting because ${ex.message}`);
+            process.exit(1);
+        }
+    }
 
     constructor(credentials){
         this.credentials = credentials;
         this.url = `mongodb+srv://${credentials.userName}:${credentials.password}@${credentials.hostname}`;
         this.client = new MongoClient(this.url);
         this.db = client.db('WorkCirle');
+        this.testConnection(this.db);
+
         this.users = this.db.collection("users");
         this.chats = this.db.collection("chats");
+        this.joinCodes = this.db.collection("joinCodes");
     }
 
 
-    isUsernameAvailable(username){
-        return false;
+    async isUsernameAvailable(username){
+        return !(await this.users.find({ username : username }));
     }
 
-    //return true when successfully created
-    createNewUser(username, passwordHash){
-        return false;
+    async createNewUser(username, passwordHash){
+        if(!this.isUsernameAvailable(username)){
+            return false;
+        }
+        const user = {username:username, passwordHash:passwordHash, chats: []};
+        await this.users.insertOne(user);
+        return true;
     }
 
-    getPasswordHash(username){
-        // get hash from db
-        return null;
+    async addGuestUserToChat(username,chatID){
+        await this.chats.updateOne({chatID:chatID}, {$push: {users:username}});
+        return true;
     }
 
-
-
-    getJoinCodes(){
-        return {};    
+    async addUsertoChat(username, chatID){
+        await this.users.updateOne({username:username}, { $addToSet : {chats : chatID}});
+        await this.chats.updateOne({chatID:chatID}, {$addToSet : {users:username}});
+        return true;
+    }
+    async removeUserChat(username, chatID){
+        await this.users.updateOne({username:username}, { $pull : {chats:chatID}});
+        await this.chats.updateOne({chatID:chatID}, {$pull : {users:username}});
+        return true;
     }
 
-    createNewChat(chatName, chatID, user, join_code){
-        return false;
+    async getUser(username){
+        return await this.users.findOne({username:username});
+    }
+    
+    async getUserChats(username){
+        return (await this.users.findOne({username:username})).chats;
     }
 
-    getChatData(username, chatID, startTime = null, endTime = null){
-        return {};
+    async getPasswordHash(username){
+        return (await this.users.findOne({username:username})).passwordHash;
     }
 
-    updateChatData(chatID, chatData){
-        return false;
+    async getJoinCodes(){   
+        return await this.joinCodes.find();
     }
 
-    deleteChat(username, chatID){
-        return false;
+    async createNewChat(chatName, chatID, user, joinCode){
+        
+    }
+
+    async getChatWithID(chatID){
+
+    }
+
+    async getChatWithJoinCode(joinCode){
+
+    }
+
+    async updateChatData(chatID, chatData){
+
+    }
+
+    async addChatMessage(username, message, chatID){
+
+    }
+
+    async deleteChat(username, chatID){
+
+    }
+
+    async #removeJoinCode(joinCode){
+
     }
 
 }
