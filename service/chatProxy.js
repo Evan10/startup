ed0ce4359d634b36b84b3a2c020ecc9d4cfbb420
Evan <evanroyal1@gmail.com>
2025-12-03 18:18:51 -0700

    continued work on user class and chat class

--- service/chatProxy.js
+++ service/chatProxy.js
@@ -1,19 +1,26 @@
 import {WebSocketServer} from "ws";
 
-TYPE_MESSAGE = "message";
-TYPE_CONNECT_TO_CHAT = "connect"
-TYPE_DISCONNECT = "disconnect";
+const TYPE_MESSAGE = "message";
+const TYPE_CONNECT_TO_CHAT = "connect"
+const TYPE_DISCONNECT = "disconnect";
+const TYPE_PING = "ping";
+const TYPE_PONG = "pong";
 
 
 export default class chatProxy{
 
     constructor(httpServer){
         this.server = httpServer;
-        this.wss = WebSocketServer(this.server);
+        this.wss = new WebSocketServer(this.server);
     }
     proxySetup(){
-        this.server.on("upgrade",(req, socket, head)=>{
+        this.server.on("upgrade", (req, socket, head)=>{
 
+
+
+            this.wss.handleUpgrade(req, socket, head,()=>{
+                wss.emit("connection", ws, req);
+            })
         })
 
     }
@@ -23,28 +30,107 @@ export default class chatProxy{
 
 }
 
+class chatRoom {
 
-
-
-class user{
-    constructor(webSocket, token){
-        this.webSocket = webSocket;
-        this.token = token;
-        this.timeout = now() + 10;
-        
+    constructor(){
+        this.users = [];    
     }
 
-    renewTimeout(){
-        this.timeout = now() + 10;
+    sendMessageToOthers(sender, message){
+        for(u in this.users){
+            if(u != sender){
+                u.sendMessage(message);
+            }
+        }
+    }
+
+    onMessage(message){
+
+    }
+
+}
+
+TIME_OUT = 60; //seconds
+class user{
+
+    PING_MESSAGE = `{"type":"${TYPE_PING}"}`;
+
+    constructor(webSocket, token, chat = null){
+        this.webSocket = webSocket;
+        this.token = token;
+        this.chat = chat;
+        this.recieveMessage = null;
+        this.pingInterval = null;
+        this.lastContact = now();
+        
+        if(chat!=null){
+            addHook();
+        }
+        setupPingPong();
+    }
+
+    setupPingPong(){
+        this.renewLastContact();
+
+        this.pingInterval = setInterval(()=>{
+            this.webSocket.send(user.PING_MESSAGE);
+        }, 10 * 1000);
+
+        this.webSocket.on("message",async (message, isBinary)=>{
+            try{
+                const msg = JSON.parse(message.toString());
+                if(msg?.type == TYPE_PONG){
+                    renewLastContact();
+
+                }
+            }catch(e){
+                console.error("invalid JSON message", e);
+            }
+        });
+    }
+
+    renewLastContact(){
+        this.lastContact = now();
     }
 
     isTimedOut(){
-        return now() >= this.timeout;
+        return this.lastContact - now() > TIME_OUT;
     }
 
-    setup(){
-        this.webSocket.on()
+    setNewChat(chat){
+        this.removeHook();
+        this.chat = chat;
+        this.addHook();
+    }
 
+    addHook(){
+        this.recieveMessage = (message, isBinary) => {
+            if(isBinary){console.log("binary message recieved");return;}
+            try{
+                const msg = JSON.parse(message.toString());
+                if(msg?.type != TYPE_PONG){
+                    this.chat.onMessage(msg);
+                }
+            }catch(e){
+                console.error("invalid JSON message", e);
+            }
+            
+        };
+
+        this.webSocket.on("message", this.recieveMessage);
+    }
+
+    removeHook(){
+        this.webSocket.off("message", this.recieveMessage);
+    }
+
+    sendMessage(message){
+        this.webSocket.send(JSON.stringify(message));
+    }
+
+    close(){
+        removeHook();
+        this.webSocket.close()
     }
 
 }
